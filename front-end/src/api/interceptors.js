1d8ab7055657890bacd7f54ac7fbfbc7f0d583c1
Cuong Bui <cuongbphv@gmail.com>
2019-03-08 11:37:14 +0700

    update setup front-end

--- front-end/src/api/interceptors.js
+++ front-end/src/api/interceptors.js
@@ -1,35 +1,108 @@
 import store from '../store'
 import camelcaseKeys from 'camelcase-keys'
-function setup (instance) {
-  instance.interceptors.request.use(function (config) {
-    const token = store.getters['auth/token']
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+// import router from '../router'
+
+function setup(instance) {
+  instance.interceptors.request.use(
+    function(config) {
+      const token = store.getters['auth/token']
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    function(err) {
+      return Promise.reject(err)
     }
-    return config
-  }, function (err) {
-    return Promise.reject(err)
-  })
+  )
 }
 
-function checkToken (instance) {
+function checkToken(instance) {
   instance.interceptors.response.use(
-    (response) => {
+    response => {
       if (response.headers['content-type'].startsWith('application/json')) {
         response.data = camelcaseKeys(response.data, { deep: true })
       }
       return response
     },
-    (error) => {
-      if (error.response.status === 401 && !window.location.pathname.includes('login')) {
+    error => {
+      if (
+        error.response.status === 401 &&
+        !window.location.pathname.includes('login')
+      ) {
         store.dispatch('auth/clear')
+        // router.push({name: 'login'})
         window.location.href = window.location.origin + '/login'
       }
       return Promise.reject(error)
     }
   )
 }
+
+function showSpinner (instance) {
+  instance.interceptors.request.use(
+    request => {
+      store.dispatch('layout/showSpinner')
+      return request
+    },
+    error => {
+      store.dispatch('layout/showSpinner')
+      return Promise.reject(error)
+    }
+  )
+}
+
+function hideSpinner(instance) {
+  instance.interceptors.response.use(
+    response => {
+      setTimeout(() => {
+        store.dispatch('layout/hideSpinner')
+      }, 500)
+      return response
+    },
+    error => {
+      setTimeout(() => {
+        store.dispatch('layout/hideSpinner')
+      }, 500)
+      return Promise.reject(error)
+    }
+  )
+}
+
+function checkError(instance) {
+  instance.interceptors.response.use(
+    response => {
+      if (response.data.status === 'error') {
+        const {data: {errorMessage: {errorCode} = {}} = {}} = response.data
+        if (errorCode && [22, 71, 73].includes(errorCode) &&
+          !window.location.pathname.includes('login')
+        ) {
+          // router.push({name: 'ErrorPage', params: { errorCode }})
+          // window.location.href = window.location.origin + '/error/' + errorCode
+        }
+
+        return Promise.reject(response.data.data.errorMessage)
+      }
+
+      return response
+    },
+    error => {
+      if (
+        [403, 404, 500, 503].includes(error.response.status) &&
+        !window.location.pathname.includes('login')
+      ) {
+        // router.push({name: 'ErrorPage', params: { errorCode: error.response.status }})
+        // window.location.href = window.location.origin + '/error/' + error.response.status
+      }
+      return Promise.reject(error)
+    }
+  )
+}
+
 export default {
   setup,
-  checkToken
+  checkToken,
+  hideSpinner,
+  showSpinner,
+  checkError
 }
---
+++ front-end/src/constants/date-picker.config.js
@@ -0,0 +1,16 @@
+const lang = {
+  days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  pickers: ['next 7 days', 'next 30 days', 'previous 7 days', 'previous 30 days'],
+  placeholder: {
+    date: 'Select Date',
+    dateRange: 'Select Date Range'
+  }
+}
+
+const format = 'YY-MM-DD'
+
+export default {
+  lang: lang,
+  format: format
+}

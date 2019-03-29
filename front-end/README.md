d4ff94377e42d97c2b7be6b7696ac850e116bcc5
Cuong Bui <cuongbphv@gmail.com>
2019-03-30 01:38:53 +0700

    update template and optimize login page

--- front-end/README.md
+++ front-end/README.md
@@ -2,28 +2,10 @@
 
 ## Project setup
 ```
-npm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+yarn run dev
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,26 +1,38 @@
-// Core of Vue
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
+
+import App from './App'
 import store from './store'
-import i18n from './lang/i18n'
-// Libraries
-import Vue2Filters from 'vue2-filters'
-import BootstrapVue from 'bootstrap-vue'
+import router from './router'
+import i18n from './lang' // Internationalization
 
-// Style for application
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+import './mock' // simulation data
 
-Vue.use(Vue2Filters)
-Vue.use(BootstrapVue)
+import * as filters from './filters' // global filters
 
-// Config for Vue instance
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  i18n,
+  el: '#app',
   router,
   store,
+  i18n,
   render: h => h(App)
-}).$mount('#app')
+})
